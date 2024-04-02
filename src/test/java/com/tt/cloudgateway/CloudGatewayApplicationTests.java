0abdf631f03794dbd23a866b79247b0dff633ff1
Tejas Tanna <tannatejas@gmail.com>
2024-04-02 17:23:07 +0100

    initial commit

---
+++ src/test/java/com/tt/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tt.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
